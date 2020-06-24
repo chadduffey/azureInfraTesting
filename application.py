e7610b4f6cf617b5032e547fd7cdec461a1a5086
Chad Duffey <7192541+chadduffey@users.noreply.github.com>
2020-06-24 07:07:08 -0700

    convert passwords file to azure blob SAS

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 import random
+import requests
 
 from flask import Flask
 
@@ -12,7 +13,10 @@ def index():
 # https://github.com/danielmiessler/SecLists/blob/master/Passwords/darkweb2017-top1000.txt
 @app.route('/password', methods=['GET'])
 def password():
-    lines = open('pwds.txt').read().splitlines() 
+    source = "https://passwordfunctionexample.blob.core.windows.net/data/pwds.txt?sv=2019-02-02&st=2020-06-24T13%3A27%3A01Z&se=2030-01-01T16%3A00%3A00Z&sr=b&sp=r&sig=7vQ9lzdataV60EpalaXeRGV6mNtzBOCZDCGWG3mjF1Y%3D"
+    response = requests.get(source)
+    data = response.text
+    lines = data.splitlines() 
     return random.choice(lines) + "\n"
 
 if __name__ == '__main__':
